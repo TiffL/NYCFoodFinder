ff7bcbdf9bc793d80eacbff2d0c90686340dbbbc
TiffL <lei.tiffany@gmail.com>
2018-12-11 17:28:58 -0500

    Updates

---
+++ precache-manifest.8858d8a90f0777216748ad36ab26289d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1877bab1c17ec5cd0863",
+    "url": "/NYCFoodFinder/static/js/runtime~main.1877bab1.js"
+  },
+  {
+    "revision": "11d9ab916fd1c86fb702",
+    "url": "/NYCFoodFinder/static/js/main.11d9ab91.chunk.js"
+  },
+  {
+    "revision": "678208e718f424dacb75",
+    "url": "/NYCFoodFinder/static/js/1.678208e7.chunk.js"
+  },
+  {
+    "revision": "11d9ab916fd1c86fb702",
+    "url": "/NYCFoodFinder/static/css/main.caaf9c2e.chunk.css"
+  },
+  {
+    "revision": "8271e6de7ec396cb43bb6fe2f3de8ff7",
+    "url": "/NYCFoodFinder/index.html"
+  }
+];
